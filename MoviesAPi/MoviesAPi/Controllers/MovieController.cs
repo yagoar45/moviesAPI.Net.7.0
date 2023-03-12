214add3d134dc48fd83e984899dd83abc9fffb77
yago-ar-45 <yago.moreira@sou.inteli.edu.br>
2023-03-12 16:17:07 -0300

    add versão 1.0

---
+++ MoviesAPi/MoviesAPi/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPi.Models;
+
+namespace MoviesAPi.Controllers;
+
+// definindo essa classe como controladora
+[ApiController]
+
+// definindo essa rota como portadora do sufixo "controller"
+[Route("[controller]")]
+
+// usando a herança para definir o controller
+public class MovieController : Controller   
+{
+    // criando lista estática na classe 
+    private static List<Movie> movies = new List<Movie>();
+
+
+    // lógica para adicionar filme no sistema
+    // a partir do parâmetro movie 
+    // define o método como post por meio do register
+    [HttpPost]
+    // o [FromBody] diz que o argumento vem da requisição web 
+    public void AddMovie([FromBody] Movie movie)
+    {
+        if (string.IsNullOrEmpty(movie.Title) && string.IsNullOrEmpty(movie.Genre) && movie.Duration >= 70)
+        {
+            movies.Add(movie);
+            Console.WriteLine($"Duração: {movie.Duration} \n" + $"Título: {movie.Title}");
+        }
+    }
+}
---
+++ MoviesAPi/MoviesAPi/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPi.Models;
+
+public class Movie
+{
+    // criando props exclusivas da classe Movie
+
+    [Required(ErrorMessage = "O título do filme é obrigatório !")]
+    [MaxLength(90, ErrorMessage = "O título só pode ter, no máximo, 90 caracteres.")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório !")]
+    [MaxLength(50, ErrorMessage = "O título só pode ter, no máximo, 50 caracteres")]
+    public string Genre { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório !")]
+    [Range(70, 600, ErrorMessage = "Para ser considerado um filme, ele deve ter entre 70 a 600 minutos.")]
+    public int Duration { get; set; }   
+}
